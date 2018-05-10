bf890ef87905476a9ee656bd544e345ace81ccfd
Emily Chen <emilychen@wirelessprv-10-195-3-120.near.illinois.edu>
2018-05-10 17:59:29 -0500

    removed the categories button

--- flaskApp-askjae/app.py
+++ flaskApp-askjae/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from functools import wraps
+import json
+from pprint import pprint
 
 app = Flask(__name__, static_folder = "tmp")
 
@@ -6,5 +9,12 @@ app = Flask(__name__, static_folder = "tmp")
 def index():
     return render_template('index.html')
 
+@app.route('/generate')
+def generate():
+	#load the file
+	#call a function that parses the file
+	return render_template('test3.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
